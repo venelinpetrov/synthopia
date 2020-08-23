a22c01b3d4602a8fc7a966e3537a4c2a95af463c
venelinpetrov <vpetrov33tu@gmail.com>
2020-08-23 21:24:40 +0300

    Play with scope

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,22 @@ import './App.css';
 
 import { withAudioContext } from './components';
 import { wave1 } from './custom_waves';
+import { drawWave } from './utils'
+
 
 function App({ ctx }: { ctx: AudioContext}) {
   const gain = ctx.createGain();
   const osc = ctx.createOscillator();
+  var analyser = ctx.createAnalyser();
+
+  //routing
 
   gain.gain.value = 0;
   osc.frequency.value = 240;
   osc.connect(gain);
-  gain.connect(ctx.destination);
+  gain.connect(analyser);
+
+  analyser.connect(ctx.destination);
 
   osc.setPeriodicWave(wave1(ctx));
   osc.start(0);
@@ -19,6 +26,9 @@ function App({ ctx }: { ctx: AudioContext}) {
   const handleStart = () => {
     gain!.gain.setValueAtTime(0, 0);
     gain!.gain.linearRampToValueAtTime(.5, ctx.currentTime + .02);
+    var c = document.getElementById('scope'),
+      canvasContext = (c as HTMLCanvasElement).getContext("2d");
+    drawWave(analyser, canvasContext!);
   };
 
   const handleStop = () => {
@@ -28,9 +38,11 @@ function App({ ctx }: { ctx: AudioContext}) {
 
   return (
     <div className="App">
-      test
-      <button onClick={handleStart}>Start</button>
-      <button onClick={handleStop}>Stop</button>
+      <div>
+        <button onClick={handleStart}>Start</button>
+        <button onClick={handleStop}>Stop</button>
+      </div>
+      <canvas id='scope'></canvas>
     </div>
   );
 }
